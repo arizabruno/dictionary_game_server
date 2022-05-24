4a731e2b3ce29036bc6c9b73fad23d6f7af365f6
arizabruno <brunoariza@outlook.com>
2022-05-24 18:55:49 -0300

    Fixing ...

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://dictionarium.surge.sh/",
     methods: [ "GET", "POST" ]
   }
 });
